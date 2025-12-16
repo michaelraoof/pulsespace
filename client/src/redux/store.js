fd59a0e54299b2a40c668cbf2960b90af80778fa
Michael Raoof <raoofmichael@yahoo.com>
2025-12-16 19:16:55 +0200

    feat(client): remove redux

--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
-
-// THE GLOBAL STORE SETUP
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
